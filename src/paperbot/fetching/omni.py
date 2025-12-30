9a6f1f67973a48c15258be8869b7d29f2041e498
desmonddibba <desmonddibba@gmail.com>
2025-12-30 18:30:38 +0100

    Installed BeautifulSoup, removed LXML

--- src/paperbot/fetching/omni.py
+++ src/paperbot/fetching/omni.py
@@ -1,34 +1,36 @@
 import requests
 from urllib.parse import urlparse, urldefrag
-from lxml import html
+from bs4 import BeautifulSoup
+
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Paperbot)"
 }
 
 
+MORGONSVEPET_URL = "https://omni.se/upptack?sok=viktigaste+nyheterna+p%C3%A5+tre+minuter"
 
-MORGONSVEPET_URL = "https://omni.se/upptack?sok=viktigaste+nyheterna+p%C3%A5+tre+minute"
-
-
-def fetch_html(url: str) -> html.HtmlComment:
+def fetch_html(url: str) -> str:
+    """" Fetch the HTML of a given"""
     response = requests.get(url, headers=HEADERS, timeout=10)
     response.raise_for_status()
-    return html.fromstring(response.content)
+    return response.text
 
 
+# TODO: Set up a filter to only fetch valid URL's from searchquery. (Morgonsvepet newsposts, NOT paid articles etc)
 def fetch_latest_post_url() -> str | None:
-    response = requests.get(MORGONSVEPET_URL, headers=HEADERS, timeout=10)
-    response.raise_for_status()
+    """Fetch the latest Morgonsvepet post URL from Omni."""
+    html_content = fetch_html(MORGONSVEPET_URL)
+    soup = BeautifulSoup(html_content, "html.parser")
 
-    tree = html.fromstring(response.content)
-    links = tree.xpath("//a[@rel='canonical']/@href")
-
-    if not links:
+    # Find the canonical link
+    link_el = soup.find("a", rel="canonical")
+    if not link_el:
         return None
-    
-    url = urldefrag(links[0])[0]
 
+    url = urldefrag(link_el.get("href"))[0]
+
+    # Make sure the URL is absolute
     if url.startswith("/"):
         parsed = urlparse(MORGONSVEPET_URL)
         url = f"{parsed.scheme}://{parsed.netloc}{url}"
